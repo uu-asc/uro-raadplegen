17f5a1715ec9bba83e8b1065a60829e08ed913eb
Lawrence Vriend <43742775+lcvriend@users.noreply.github.com>
2020-08-05 01:48:50 +0200

    Add basic search

--- interface.js
+++ interface.js
@@ -1,5 +1,91 @@
 console.log("i have been summoned")
 
-fetch("data.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const info   = document.getElementById('info');
+
+let uro_data;
+fetch("https://raw.githubusercontent.com/uu-asc/uro_raadplegen/master/data.json")
+    .then(response => response.json())
+    .then(json => uro_data = json);
+
+
+// SEARCH RECORDS
+search.addEventListener('keyup', filterRecords);
+function filterRecords() {
+    let filter = search.value.toUpperCase();
+    if (uro_data.hasOwnProperty(filter)) {
+        let record = uro_data[filter]
+        result.innerHTML = `
+            <div class="container">
+                <div class="box">
+                    <h2>opleiding</h2>
+                    <div class="fields">
+                        <div class="field">opleiding</div>
+                        <div class="value">${filter}</div>
+                        <div class="field">opleiding_naam</div>
+                        <div class="value">${record['opleiding_naam']}</div>
+                        <div class="field">type_opleiding</div>
+                        <div class="value">${record['type_opleiding']}</div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2>croho</h2>
+                    <div class="fields">
+                        <div class="field">croho</div>
+                        <div class="value">${record['croho']}</div>
+                        <div class="field">croho_naam</div>
+                        <div class="value">${record['croho_naam']}</div>
+                        <div class="field">croho_sector</div>
+                        <div class="value">${record['croho_sector']}</div>
+                        <div class="field">nominale_studieduur</div>
+                        <div class="value">${record['nominale_studieduur']}</div>
+                        <div class="field">bekostigings_niveau</div>
+                        <div class="value">${record['bekostigings_niveau']}</div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2>onderdeel</h2>
+                    <div class="fields">
+                        <div class="field">faculteit</div>
+                        <div class="value">${record['faculteit']}</div>
+                        <div class="field">aggregaat_1</div>
+                        <div class="value">${record['aggregaat_1']}</div>
+                        <div class="field">aggregaat_2</div>
+                        <div class="value">${record['aggregaat_2']}</div>
+                        <div class="field">aggregaat_3</div>
+                        <div class="value">${record['aggregaat_3']}</div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2>instroom</h2>
+                    <div class="fields">
+                        <div class="field">februari_instroom</div>
+                        <div class="value">${record['februari_instroom']}</div>
+                        <div class="field">inschrijven_tm</div>
+                        <div class="value">${record['inschrijven_tm']}</div>
+                        <div class="field">herinschrijven_tm</div>
+                        <div class="value">${record['herinschrijven_tm']}</div>
+                        <div class="field">inschrijven_vanaf</div>
+                        <div class="value">${record['inschrijven_vanaf']}</div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2>toelating</h2>
+                    <div class="fields">
+                        <div class="field">NT</div>
+                        <div class="value">${record['NT']}</div>
+                        <div class="field">NG</div>
+                        <div class="value">${record['NG']}</div>
+                        <div class="field">EM</div>
+                        <div class="value">${record['EM']}</div>
+                        <div class="field">CM</div>
+                        <div class="value">${record['CM']}</div>
+                    </div>
+                </div>
+            </div>
+            `;
+    } else {
+        result.innerHTML = ''
+    };
+};
