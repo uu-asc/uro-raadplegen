fe17d36f18f4d734e6f1d7f18b097680184f3807
lcvriend <43742775+lcvriend@users.noreply.github.com>
2024-10-06 16:22:23 +0200

    Update collegegeld interface

--- js/interface_collegegeld.js
+++ js/interface_collegegeld.js
@@ -1,263 +1,91 @@
-let data = {
-    "columns": [
-        "BA",
-        "MA"
-    ],
-    "index": [
-        [
-            "wettelijk",
-            "Deeltijd"
-        ],
-        [
-            "wettelijk",
-            "Voltijd"
-        ],
-        [
-            "kleinschalig",
-            "UCR"
-        ],
-        [
-            "kleinschalig",
-            "PPE"
-        ],
-        [
-            "kleinschalig",
-            "UCU"
-        ],
-        [
-            "niet-EER",
-            "UCR"
-        ],
-        [
-            "niet-EER",
-            "LAAG"
-        ],
-        [
-            "niet-EER",
-            "PPE"
-        ],
-        [
-            "niet-EER",
-            "HOOG"
-        ],
-        [
-            "niet-EER",
-            "UCU"
-        ],
-        [
-            "niet-EER",
-            "TOP"
-        ],
-        [
-            "graad",
-            "LAAG"
-        ],
-        [
-            "graad",
-            "PPE"
-        ],
-        [
-            "graad",
-            "HOOG"
-        ],
-        [
-            "graad",
-            "UCU"
-        ],
-        [
-            "graad",
-            "TOP"
-        ]
-    ],
-    "data": [
-        [
-            2056,
-            2056
-        ],
-        [
-            2314,
-            2314
-        ],
-        [
-            4135,
-            null
-        ],
-        [
-            4628,
-            null
-        ],
-        [
-            4802,
-            null
-        ],
-        [
-            9985,
-            null
-        ],
-        [
-            11523,
-            18332
-        ],
-        [
-            13723,
-            null
-        ],
-        [
-            14665,
-            21736
-        ],
-        [
-            14665,
-            null
-        ],
-        [
-            24408,
-            27969
-        ],
-        [
-            10365,
-            12879
-        ],
-        [
-            12637,
-            null
-        ],
-        [
-            12922,
-            15045
-        ],
-        [
-            12922,
-            null
-        ],
-        [
-            21160,
-            24303
-        ]
-    ]
+const simpleTable = document.querySelector("simple-table")
+const studyPeriodSelector = document.querySelector("study-period-selector")
+const flexinput = document.querySelector(`#aantal-ec-flex`)
+const flexoutput = document.querySelector(`#te-betalen-bedrag-flex`)
+
+let STATE
+let FORMAT_OPTIONS = getFormatOptions()
+let LOCALE = getLocale()
+
+document.getElementById("collegejaar").addEventListener("change", event => {
+    const newSrc = `../data/collegegeld/bedragen_${event.target.value}.json`
+    simpleTable.setAttribute("src" , newSrc)
+})
+
+simpleTable.addEventListener("data-loaded", () => {
+    STATE = simpleTable.getValues()
+    updateFlexFee()
+})
+
+simpleTable.addEventListener("cell-click", event => {
+    const content = event.detail.value
+
+    navigator
+    .clipboard.writeText(content)
+    .then(res => {
+        messageboard.innerHTML = `<span class="heart">&hearts;</span> klembord: <span class="klembord">${content}</span> <span class="heart">&hearts;</span>`
+        setTimeout(() => messageboard.innerHTML = "", 1200)
+    })
+})
+
+studyPeriodSelector.addEventListener("period-change", event => {
+    updateTable(event.detail.n)
+})
+
+function getFormatOptions() {
+    const checkboxAsCurrency = document.querySelector(`[name="as-currency"]`)
+    const checkboxSepThousands = document.querySelector('input[name="sep-thousands"]')
+    const formatOptions = {
+        "style": checkboxAsCurrency.checked ? "currency" : "decimal",
+        "currency": "EUR",
+        "useGrouping": checkboxSepThousands.checked,
+        "minimumFractionDigits": 2,
+        "maximumFractionDigits": 2,
+        "trailingZeroDisplay": !checkboxAsCurrency.checked ? "stripIfInteger" : "auto",
+    }
+    return formatOptions
 }
 
-let maanden = {
-    "01": {"nl-NL": "september", "en-US": "September"},
-    "02": {"nl-NL": "oktober", "en-US": "October"},
-    "03": {"nl-NL": "november", "en-US": "November"},
-    "04": {"nl-NL": "december", "en-US": "December"},
-    "05": {"nl-NL": "januari", "en-US": "January"},
-    "06": {"nl-NL": "februari", "en-US": "February"},
-    "07": {"nl-NL": "maart", "en-US": "March"},
-    "08": {"nl-NL": "april", "en-US": "April"},
-    "09": {"nl-NL": "mei", "en-US": "May"},
-    "10": {"nl-NL": "juni", "en-US": "June"},
-    "11": {"nl-NL": "juli", "en-US": "July"},
-    "12": {"nl-NL": "augustus", "en-US": "August"},
+function getLocale() {
+    return document.querySelector('input[type="radio"]:checked')?.value
 }
 
-let selectStartmaand = document.querySelector(`[name="startmaand"]`)
-let selectStopmaand = document.querySelector(`[name="stopmaand"]`)
-let labelStartmaand = document.querySelector(`#label-startmaand`)
-let labelStopmaand = document.querySelector(`#label-stopmaand`)
-let inputNMaanden = document.querySelector(`[name="nmaanden"]`)
-let checkboxAsCurrency = document.querySelector(`[name="ascurrency"]`)
-let radioLocaleNL = document.querySelector(`#locale-nl`)
-let tabel = document.querySelector("table")
-let flexinput = document.querySelector(`#aantal-ec-flex`)
-let flexoutput = document.querySelector(`#te-betalen-bedrag-flex`)
+function updateTable(n) {
+    const calculateFee = value => value ? (value / 12) * n : null
+    const newValues = STATE.map(row => row.map(calculateFee))
+    simpleTable.updateValues(newValues)
+}
+
+document.querySelectorAll('input[name="locale"]').forEach(radio => {
+    radio.addEventListener("change", () => {
+        LOCALE = getLocale()
+        simpleTable.setAttribute("locale", LOCALE)
+        studyPeriodSelector.setAttribute("language", LOCALE.substr(0, 2))
+    })
+})
+
+function handleFormattingChange() {
+    FORMAT_OPTIONS = getFormatOptions()
+    simpleTable.setFormatOptions([FORMAT_OPTIONS, FORMAT_OPTIONS])
+}
+
+document.getElementById("as-currency").addEventListener("change", handleFormattingChange)
+document.getElementById("sep-thousands").addEventListener("change", handleFormattingChange)
 
 function calcFlexFee(ec) {
-    return formatNumber(ec * (2209 / 60) * 1.15)
+    return (ec * (STATE[1][0] / 60) * 1.15).toLocaleString(LOCALE, FORMAT_OPTIONS)
 }
 
 function updateFlexFee() {
     if ( flexinput.value ) {
         flexoutput.innerText = calcFlexFee(flexinput.value)
-    } else { flexoutput.innerText = "" }
-    return
-}
-
-function updateLabelsMonths() {
-    let locale = getLocale()
-    let start = selectStartmaand.value
-    let stop = selectStopmaand.value
-    labelStartmaand.innerText = maanden[start][locale]
-    labelStopmaand.innerText = maanden[stop][locale]
-}
-
-function getLocale() {
-    return document.querySelector('input[name="locale"]:checked').value
-}
-
-function formatNumber(i) {
-    let locale = getLocale()
-    if ( i === 0 ) { return "" }
-    if ( !checkboxAsCurrency.checked ) {
-        let options = {
-            "trailingZeroDisplay": "stripIfInteger",
-            "useGrouping": document.querySelector('input[name="septhousands"]').checked,
-            "minimumFractionDigits": 2,
-            "maximumFractionDigits": 2,
-        }
-        return i.toLocaleString(locale, options)
-    }
-    let options = {
-        "currency": "EUR",
-        "style": "currency",
-        "useGrouping": document.querySelector('input[name="septhousands"]').checked,
-    }
-    return i.toLocaleString(locale, options)
-}
-
-function updateTable(data) {
-    let nmaanden = parseInt(inputNMaanden.value)
-    Array.from(tabel.tBodies[0].rows).forEach((row, rowidx) => {
-        let cells = row.querySelectorAll("td")
-        cells.forEach((cell, colidx) => {
-            let bedrag = ( data[rowidx][colidx] / 12 ) * nmaanden
-            cell.innerText = formatNumber(bedrag)
-        })
-    })
-}
-
-function updateInputsMaanden() {
-    Array.from(selectStopmaand.options).forEach(option => {
-        if ( option.value < selectStartmaand.value ) {
-            option.setAttribute("hidden", "")
-        } else {
-            option.removeAttribute("hidden", "")
-        }
-    })
-
-    Array.from(selectStartmaand.options).forEach(option => {
-        if ( option.value > selectStopmaand.value ) {
-            option.setAttribute("hidden", "")
-        } else {
-            option.removeAttribute("hidden", "")
-        }
-    })
-
-    let diff = parseInt(selectStopmaand.value) - parseInt(selectStartmaand.value) + 1
-    inputNMaanden.value = diff
-    updateTable(data.data)
-}
-
-function incrementMaand(n) {
-    let val = parseInt(inputNMaanden.value) + n
-    return val > 12 ? 12 : val < 1 ? 1 : val
+    } else { flexoutput.innerText = "" } return
 }
 
 document.addEventListener("change", event => {
-    if ( event.target.matches(`[name$=maand]`) ) {
-        updateInputsMaanden()
-        updateLabelsMonths()
-        return
-    }
-    if ( event.target.matches(`[name="nmaanden"]`) ) {
-        updateTable(data.data)
-        return
-    }
     if ( event.target.closest("fieldset")?.classList.contains("getalsnotatie") ) {
-        updateTable(data.data)
+        updateTable(studyPeriodSelector.n)
         updateFlexFee()
-        updateLabelsMonths()
     }
     if ( event.target.matches(`#aantal-ec-flex`)) {
         updateFlexFee()
@@ -267,14 +95,5 @@ document.addEventListener("change", event => {
 document.addEventListener("keyup", event => {
     if ( event.target.matches(`#aantal-ec-flex`)) {
         updateFlexFee()
-    }
-})
-
-document.addEventListener("click", event => {
-    if ( event.target.matches(`#increment, #decrement`) ) {
-        let n = event.target.id === "increment" ? 1 : -1
-        let nmaand = incrementMaand(n)
-        inputNMaanden.value = nmaand
-        updateTable(data.data)
     }
 })
